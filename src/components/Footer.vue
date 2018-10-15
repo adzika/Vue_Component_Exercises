1c970981a9e98e64c99dbbd07a2897aa1f5edc51
Ada <adziklipa@gmail.com>
2018-10-15 19:34:51 +0100

    Finished exercise

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -10,7 +10,7 @@
 
 <script>
     export default {
-        name: "Footer.vue"
+        name: "Footer"
     }
 </script>
 
--- src/components/ServerDetails.vue
+++ src/components/ServerDetails.vue
@@ -1,12 +1,32 @@
 <template>
     <div class="col-xs-12 col-sm-6">
-        <p>Server Details are currently not updated</p>
+        <p v-if="!server">Please select a server</p>
+        <p v-else>Server #{{ server.id }} selected, Status: {{ server.status }}</p>
+        <hr>
+        <button @click="resetStatus">Change to normal</button>
     </div>
 </template>
 
 <script>
+    import { serverBus } from "../main.js";
+
     export default {
-        name: "ServerDetails.vue"
+        data() {
+            return {
+                server: null
+            }
+        },
+        name: "ServerDetails",
+        methods: {
+            resetStatus() {
+                this.server.status = 'Normal';
+            }
+        },
+        created() {
+            serverBus.$on('selectedServer', (server) => {
+                this.server = server;
+            });
+        }
     }
 </script>
 
